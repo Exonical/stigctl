5d9e296e0e2825a506357302c675b2d2aab5bc0a
Exonical <brycemanglin@gmail.com>
2026-09-19 16:29:12 -0700

    feat: add Goss effective-rule vars

---
+++ internal/validation/vars.go
@@ -0,0 +1,25 @@
+package validation
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Vars struct {
+	Profile      string          `yaml:"profile"`
+	Hostname     string          `yaml:"hostname"`
+	EnabledRules map[string]bool `yaml:"enabled_rules"`
+}
+
+func WriteVars(path string, vars Vars) error {
+	data, err := yaml.Marshal(vars)
+	if err != nil {
+		return fmt.Errorf("marshal Goss vars: %w", err)
+	}
+	if err := os.WriteFile(path, data, 0o600); err != nil {
+		return fmt.Errorf("write Goss vars: %w", err)
+	}
+	return nil
+}
