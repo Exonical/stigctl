f294f865ebf87593c37bd86b6ba1173e6e39458b
Exonical <brycemanglin@gmail.com>
2026-09-19 16:29:28 -0700

    feat: generate effective Goss vars for exceptions

--- internal/cli/common.go
+++ internal/cli/common.go
@@ -1,8 +1,49 @@
 package cli
 
-import stigprofile "github.com/Exonical/stigctl/internal/profile"
+import (
+	"os"
+	"time"
+
+	"github.com/Exonical/stigctl/internal/exceptions"
+	"github.com/Exonical/stigctl/internal/policy"
+	stigprofile "github.com/Exonical/stigctl/internal/profile"
+	"github.com/Exonical/stigctl/internal/rules"
+	"github.com/Exonical/stigctl/internal/validation"
+)
 
 func validateProfile(id string) error {
 	_, err := stigprofile.NewStore(contentRoot).Resolve(id)
 	return err
+}
+
+func writeEffectiveGossVars(
+	ruleDoc rules.Document,
+	exceptionDoc exceptions.Document,
+	profile string,
+	hostname string,
+) (string, func(), error) {
+	file, err := os.CreateTemp("", "stigctl-goss-vars-*.yaml")
+	if err != nil {
+		return "", func() {}, err
+	}
+	path := file.Name()
+	if err := file.Close(); err != nil {
+		_ = os.Remove(path)
+		return "", func() {}, err
+	}
+
+	enabled := policy.EnabledRules(ruleDoc, exceptionDoc, policy.Context{
+		Profile:  profile,
+		Hostname: hostname,
+		Now:      time.Now(),
+	})
+	if err := validation.WriteVars(path, validation.Vars{
+		Profile:      profile,
+		Hostname:     hostname,
+		EnabledRules: enabled,
+	}); err != nil {
+		_ = os.Remove(path)
+		return "", func() {}, err
+	}
+	return path, func() { _ = os.Remove(path) }, nil
 }
