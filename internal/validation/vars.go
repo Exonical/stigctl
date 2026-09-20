dad4798182b45d54db5341fad171e52c48be65b4
Exonical <brycemanglin@gmail.com>
2026-09-19 19:20:35 -0700

    feat: expose shared STIG validation helper to Goss

--- internal/validation/vars.go
+++ internal/validation/vars.go
@@ -10,6 +10,7 @@ import (
 type Vars struct {
 	Profile      string          `yaml:"profile"`
 	Hostname     string          `yaml:"hostname"`
+	CheckScript  string          `yaml:"check_script,omitempty"`
 	EnabledRules map[string]bool `yaml:"enabled_rules"`
 }
 
